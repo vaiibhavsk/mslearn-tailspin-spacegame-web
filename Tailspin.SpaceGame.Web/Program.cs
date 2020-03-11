75af1516b1f63fecca639aaa73cd6857acab8e38
vaibhavkolhe1996@gmail.com <vaibhavkolhe1996@gmail.com>
2020-03-11 12:37:20 +0530

    this is test message

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -8,12 +8,14 @@ namespace TailSpin.SpaceGame.Web
         public static void Main(string[] args)
         {
             //this is netcore project 
-           
+           //this is another project
+           //this is one another project
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        //testing
     }
 }
